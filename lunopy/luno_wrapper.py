3531cc2331420c9503cc863fcafd096c49c3f9c4
Meezaan Sulaiman <slmmuh005@gmail.com>
2018-05-09 22:19:15 +0200

    adding property decorator to Main class methods

--- lunopy/luno_wrapper.py
+++ lunopy/luno_wrapper.py
@@ -7,8 +7,6 @@ import asyncio
 
 class Luno:
 
-   
-
     def __init__(self, KEY, SECRET, ACCOUNTID):
         self.KEY = KEY
         self.SECRET = SECRET
@@ -20,33 +18,41 @@ class Luno:
         self._trades = None
         self._transactions = None
 
+    @property
     def accounts(self):
+        """
+        Account related information
+        :return:
+        """
         if self._accounts is None:
             self._accounts = Accounts(self.KEY, self.SECRET, self.ACCOUNTID)
 
         return self._accounts
 
+    @property
     def orders(self):
         if self._orders is None:
             self._orders = Orders(self.KEY, self.SECRET, self.ACCOUNTID)
 
         return self._orders
 
+    @property
     def prices(self):
         if self._prices is None:
             self._prices = Prices(self.KEY, self.SECRET, self.ACCOUNTID)
 
         return self._prices
 
+    @property
     def trades(self):
         if self._trades is None:
             self._trades = Trades(self.KEY, self.SECRET, self.ACCOUNTID)
 
         return self._trades
 
+    @property
     def transactions(self):
         if self._transactions is None:
             self._transactions = Transactions(self.KEY, self.SECRET, self.ACCOUNTID)
 
         return self._transactions
-    
